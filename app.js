b8b5ac8f1109e63d1457ce0b197386de743cc1b1
slowrabbit216 <114823623+slowrabbit216@users.noreply.github.com>
2022-10-18 01:02:06 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById('submitbutton').addEventListener('click', function () {
+  document.getElementById('modal').style.display = "none";
+  
+});
+
+document.getElementById('showbutton').addEventListener('click', function () {
+  document.getElementById('modal').style.display = "flex";
+  
+});
